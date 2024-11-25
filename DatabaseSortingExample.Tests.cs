800a251cd5cc5fe264426333f7cef88ac5418043
Daniil Cheban <144154258+DaCheb1@users.noreply.github.com>
2024-11-25 19:22:42 +0300

    Add files via upload

---
+++ DatabaseSortingExample.Tests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace _25._11._2024
+{
+    [TestClass]
+    public class DatabaseOperationsTests
+    {
+        private DatabaseOperations _databaseOperations;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Подключение к Вашей тестовой базе данных
+            _databaseOperations = new DatabaseOperations("YourConnectionStringHere");
+        }
+
+        [TestMethod]
+        public void TestBubbleSort()
+        {
+            // Данные для тестирования
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Charlie" },
+                new Customer { Id = 2, Name = "Alice" },
+                new Customer { Id = 3, Name = "Bob" }
+            };
+
+            var sortedCustomers = _databaseOperations.BubbleSort(customers);
+
+            Assert.AreEqual("Alice", sortedCustomers[0].Name);
+            Assert.AreEqual("Bob", sortedCustomers[1].Name);
+            Assert.AreEqual("Charlie", sortedCustomers[2].Name);
+        }
+
+        [TestMethod]
+        public void TestSelectionSort()
+        {
+            // Данные для тестирования
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Charlie" },
+                new Customer { Id = 2, Name = "Alice" },
+                new Customer { Id = 3, Name = "Bob" }
+            };
+
+            var sortedCustomers = _databaseOperations.SelectionSort(customers);
+
+            Assert.AreEqual("Alice", sortedCustomers[0].Name);
+            Assert.AreEqual("Bob", sortedCustomers[1].Name);
+            Assert.AreEqual("Charlie", sortedCustomers[2].Name);
+        }
+    }
+}
+
---
+++ DatabaseSortingExample.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+
+
+namespace _25._11._2024
+{
+    public class DatabaseOperations
+    {
+        private DataContext _context;
+
+        public DatabaseOperations(string connectionString)
+        {
+            _context = new DataContext(connectionString);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.GetTable<Customer>().ToList();
+        }
+
+        // Пузырьковая сортировка
+        public List<Customer> BubbleSort(List<Customer> customers)
+        {
+            int n = customers.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (customers[j].Name.CompareTo(customers[j + 1].Name) > 0)
+                    {
+                        // Swap
+                        var temp = customers[j];
+                        customers[j] = customers[j + 1];
+                        customers[j + 1] = temp;
+                    }
+                }
+            }
+            return customers;
+        }
+
+        // Сортировка выбором
+        public List<Customer> SelectionSort(List<Customer> customers)
+        {
+            int n = customers.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (customers[j].Name.CompareTo(customers[minIndex].Name) < 0)
+                    {
+                        minIndex = j;
+                    }
+                }
+                // Swap
+                var temp = customers[minIndex];
+                customers[minIndex] = customers[i];
+                customers[i] = temp;
+            }
+            return customers;
+        }
+    }
+
+    // Пример класса Customer
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
+
